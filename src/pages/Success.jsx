81fb139d3ffff7e1c079f14ff8c070db14744452
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-04 13:03:28 +0000

    🌿: Sunday, June 4, 2023 at 4:03:28 PM Eastern European Summer Time

---
+++ src/pages/Success.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Success = () => {
+  return (
+    <>
+      <div className="flex flex-col w-full min-h-screen overflow-hidden"></div>
+    </>
+  );
+};
+
+export default Success;
