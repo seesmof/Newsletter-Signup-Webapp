32663526eee5770cb3c62d286f5548cf7632b912
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-04 09:46:29 +0300

    modified Main.jsx in pages

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import illustrationSignpMobile from "/illustration-sign-up-mobile.svg";
 
 const Main = () => {
   return (
     <div className="flex flex-col w-full min-h-screen overflow-hidden">
       <div className="w-full">
         <img
-          src={illustrationSignpMobile}
+          src="illustration-sign-up-mobile.svg"
           alt="sign up illustration mobile"
           className="w-full"
         />
