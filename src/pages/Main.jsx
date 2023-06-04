7e3d0e241019810e7ae842b60d0fbcc9f0c41c20
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-04 09:35:33 +0300

    changed 17 files

---
+++ src/pages/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="flex flex-col w-full min-h-screen overflow-hidden">
+      <div className="w-full">
+        <img
+          src="/illustration-sign-up-mobile.svg"
+          alt="sign up illustration mobile"
+          className="w-full"
+        />
+      </div>
+      <div className="container flex flex-col flex-1 p-6 mx-auto bg-white"></div>
+    </div>
+  );
+};
+
+export default Main;
