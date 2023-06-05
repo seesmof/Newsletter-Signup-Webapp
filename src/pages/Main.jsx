fc982e21fb8c577449be4b888733ba5015ef51d9
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-05 14:38:46 +0000

    🌿: Monday, June 5, 2023 at 5:38:46 PM Eastern European Summer Time

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import illustrationSignpMobile from "/illustration-sign-up-mobile.svg";
 import Hero from "../components/Hero";
-import Form from "../components/Form";
 import Success from "../components/Success";
 
 const Main = () => {
+  const [emailAddress, setEmailAddress] = useState("");
+
   return (
     <div className="flex flex-col w-full min-h-screen overflow-hidden">
-      <Success className="block" />
+      <Success className="block">{emailAddress}</Success>
 
       <article className="block">
         <div className="w-full">
@@ -19,7 +20,24 @@ const Main = () => {
         </div>
         <div className="container flex flex-col justify-center flex-1 p-6 mx-auto bg-white">
           <Hero />
-          <Form />
+          <form action="" className="flex flex-col justify-center">
+            <div className="flex flex-col">
+              <label
+                htmlFor="email-input"
+                className="text-slate-800 pb-2 text-sm font-bold"
+              >
+                Email address
+              </label>
+              <input
+                type="email"
+                placeholder="email@company.com"
+                className="px-6 py-2 mb-4 text-lg border-2 border-gray-300 rounded-md"
+                id="email-input"
+                required
+              />
+              <Button type={"submit"}>Subscribe to monthly newsletter</Button>
+            </div>
+          </form>{" "}
         </div>
       </article>
     </div>
