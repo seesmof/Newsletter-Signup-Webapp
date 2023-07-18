591ecac5709b9d66bb6fd45bd638d1c15e2d337d
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-18 21:31:33 +0300

    modified App.jsx in src

--- src/App.jsx
+++ src/App.jsx
@@ -36,63 +36,65 @@ function App() {
   return (
     <>
       <div className="min-h-screen bg-background w-full flex items-center justify-center">
-        {emailSent && <Confirmation email={email.current.value} />}
+        {emailSent ? (
+          <Confirmation email={email.current.value} />
+        ) : (
+          <>
+            <div className="bg-white lg:rounded-xl h-screen w-full">
+              <div className="flex flex-col h-full">
+                <img src={HeroMobile} alt="" />
 
-        {!emailSent && (
-          <div className="bg-white lg:rounded-xl h-screen w-full">
-            <div className="flex flex-col h-full">
-              <img src={HeroMobile} alt="" />
+                <section className="flex flex-col py-6 px-4 space-y-4">
+                  <h1 className="font-bold text-4xl">Stay updated!</h1>
+                  <p>
+                    Join 60,000+ product managers receiving monthly updates on:
+                  </p>
+                  <ul className="flex flex-col space-y-3">
+                    <li className="flex flex-row items-center gap-3">
+                      <img src={BulletItem} alt="" />
+                      Product discovery and building what matters
+                    </li>
+                    <li className="flex flex-row items-center gap-3">
+                      <img src={BulletItem} alt="" />
+                      Measuring to ensure updates are a success
+                    </li>
+                    <li className="flex flex-row items-center gap-3">
+                      <img src={BulletItem} alt="" />
+                      And much more!
+                    </li>
+                  </ul>
+                </section>
 
-              <section className="flex flex-col py-6 px-4 space-y-4">
-                <h1 className="font-bold text-4xl">Stay updated!</h1>
-                <p>
-                  Join 60,000+ product managers receiving monthly updates on:
-                </p>
-                <ul className="flex flex-col space-y-3">
-                  <li className="flex flex-row items-center gap-3">
-                    <img src={BulletItem} alt="" />
-                    Product discovery and building what matters
-                  </li>
-                  <li className="flex flex-row items-center gap-3">
-                    <img src={BulletItem} alt="" />
-                    Measuring to ensure updates are a success
-                  </li>
-                  <li className="flex flex-row items-center gap-3">
-                    <img src={BulletItem} alt="" />
-                    And much more!
-                  </li>
-                </ul>
-              </section>
-
-              <form
-                className="flex flex-col px-4 py-2"
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  if (email.current.value) {
-                    setEmailSent(true);
-                  }
-                }}
-              >
-                <label
-                  htmlFor="email"
-                  className="text-sm font-bold mb-2 accent-orange-500"
+                <form
+                  className="flex flex-col px-4 py-2"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    if (email.current.value) {
+                      setEmailSent(true);
+                    }
+                  }}
                 >
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  className="w-full ring-2 ring-slate-200 rounded-lg px-4 py-2"
-                  placeholder="email@company.com"
-                  required
-                  ref={email}
-                />
-                <button className="button mt-4">
-                  Subscribe to monthly newsletter
-                </button>
-              </form>
+                  <label
+                    htmlFor="email"
+                    className="text-sm font-bold mb-2 accent-orange-500"
+                  >
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="w-full ring-2 ring-slate-200 rounded-lg px-4 py-2"
+                    placeholder="email@company.com"
+                    required
+                    ref={email}
+                  />
+                  <button className="button mt-4">
+                    Subscribe to monthly newsletter
+                  </button>
+                </form>
+              </div>
             </div>
-          </div>
+          </>
         )}
       </div>
     </>
