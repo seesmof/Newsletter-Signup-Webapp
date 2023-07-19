7bdfd97f712fa7dfbfd6ece366047bc2c5424797
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-19 10:18:18 +0300

    feat: add correct version for tablets

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,25 @@ function App() {
   const Confirmation = ({ email = "your inbox" }) => {
     return (
       <>
-        <div className="h-screen w-full bg-white flex-col flex">
-          <div className="flex flex-col p-4 h-full justify-between">
-            <div className="flex flex-col space-y-5 py-24">
-              <img src={BulletItem} alt="" className="w-16 h-16" />
-              <h2 className="font-bold text-3xl">Thanks for subscribing!</h2>
-              <p>
-                A confirmation email has been sent to{" "}
-                <span className="font-bold">{email}</span>. Please open it and
-                click the button inside to confirm your subscription
-              </p>
+        <div className="min-h-screen bg-background font-medium w-full flex items-center justify-center">
+          <div className="h-screen w-full bg-white flex-col flex">
+            <div className="flex flex-col p-4 h-full justify-between">
+              <div className="flex flex-col space-y-5 py-24">
+                <img src={BulletItem} alt="" className="w-16 h-16" />
+                <h2 className="font-bold text-3xl">Thanks for subscribing!</h2>
+                <p>
+                  A confirmation email has been sent to{" "}
+                  <span className="font-bold">{email}</span>. Please open it and
+                  click the button inside to confirm your subscription
+                </p>
+              </div>
+              <button
+                className="button self-end"
+                onClick={() => setEmailSent(false)}
+              >
+                Dismiss message
+              </button>
             </div>
-            <button
-              className="button self-end"
-              onClick={() => setEmailSent(false)}
-            >
-              Dismiss message
-            </button>
           </div>
         </div>
       </>
@@ -37,84 +39,92 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen bg-background font-medium w-full flex items-center justify-center">
-        {emailSent ? (
-          <Confirmation email={email.current.value} />
-        ) : (
-          <>
-            <div className="bg-white lg:rounded-xl h-screen w-full">
-              <div className="flex flex-col h-full">
-                <img src={HeroMobile} alt="" />
+      {emailSent ? (
+        <Confirmation email={email.current.value} />
+      ) : (
+        <>
+          <div className="min-h-screen bg-background font-medium w-full flex items-center justify-center">
+            <div className="bg-white lg:rounded-xl h-screen lg:h-min lg:w-max lg:p-6 w-full">
+              <div className="flex flex-col lg:flex-row-reverse h-full">
+                <img
+                  src={HeroMobile}
+                  alt=""
+                  className="block lg:hidden object-cover object-bottom md:h-[46vh] w-full"
+                />
+                <img src={HeroDesktop} alt="" className="hidden lg:block" />
 
-                <section className="flex flex-col p-6 py-8 space-y-4">
-                  <h1 className="font-bold text-4xl">Stay updated!</h1>
-                  <p>
-                    Join 60,000+ product managers receiving monthly updates on:
-                  </p>
-                  <ul className="flex flex-col space-y-3">
-                    <li className="flex flex-row items-center gap-3">
-                      <img src={BulletItem} alt="" />
-                      Product discovery and building what matters
-                    </li>
-                    <li className="flex flex-row items-center gap-3">
-                      <img src={BulletItem} alt="" />
-                      Measuring to ensure updates are a success
-                    </li>
-                    <li className="flex flex-row items-center gap-3">
-                      <img src={BulletItem} alt="" />
-                      And much more!
-                    </li>
-                  </ul>
-                </section>
+                <div className="flex flex-col lg:justify-center">
+                  <section className="flex flex-col p-6 py-8 space-y-4">
+                    <h1 className="font-bold text-4xl">Stay updated!</h1>
+                    <p>
+                      Join 60,000+ product managers receiving monthly updates
+                      on:
+                    </p>
+                    <ul className="flex flex-col space-y-3">
+                      <li className="flex flex-row items-center gap-3">
+                        <img src={BulletItem} alt="" />
+                        Product discovery and building what matters
+                      </li>
+                      <li className="flex flex-row items-center gap-3">
+                        <img src={BulletItem} alt="" />
+                        Measuring to ensure updates are a success
+                      </li>
+                      <li className="flex flex-row items-center gap-3">
+                        <img src={BulletItem} alt="" />
+                        And much more!
+                      </li>
+                    </ul>
+                  </section>
 
-                <form
-                  className="flex flex-col px-4 py-2"
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    if (email.current.value) {
-                      if (emailRegex.test(email.current.value)) {
-                        setEmailSent(true);
-                        setEmailError(false);
-                      } else {
-                        setEmailError(true);
+                  <form
+                    className="flex flex-col px-4 py-2 md:pb-8"
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      if (email.current.value) {
+                        if (emailRegex.test(email.current.value)) {
+                          setEmailSent(true);
+                          setEmailError(false);
+                        } else {
+                          setEmailError(true);
+                        }
                       }
-                    }
-                  }}
-                >
-                  <div className="flex w-full flex-row justify-between">
-                    <label
-                      htmlFor="email"
-                      className="text-sm font-bold mb-2 accent-orange-500"
-                    >
-                      Email address
-                    </label>
-                    {emailError && (
-                      <span className="text-sm text-orange-600">
-                        Valid email required
-                      </span>
-                    )}
-                  </div>
+                    }}
+                  >
+                    <div className="flex w-full flex-row justify-between">
+                      <label
+                        htmlFor="email"
+                        className="text-sm font-bold mb-2 accent-orange-500"
+                      >
+                        Email address
+                      </label>
+                      {emailError && (
+                        <span className="text-sm text-orange-600">
+                          Valid email required
+                        </span>
+                      )}
+                    </div>
 
-                  <input
-                    type="email"
-                    id="email"
-                    className={`w-full ring-2 ring-slate-200 rounded-lg px-4 py-2 font-normal ${
-                      emailError &&
-                      "ring-orange-600 bg-orange-200/50 text-orange-600"
-                    }`}
-                    placeholder="email@company.com"
-                    required
-                    ref={email}
-                  />
-                  <button className="button mt-6">
-                    Subscribe to monthly newsletter
-                  </button>
-                </form>
+                    <input
+                      type="email"
+                      id="email"
+                      className={`w-full ring-2 ring-slate-200 rounded-lg px-4 py-2 font-normal ${
+                        emailError &&
+                        "ring-orange-600 bg-orange-200/50 text-orange-600"
+                      }`}
+                      placeholder="email@company.com"
+                      required
+                      ref={email}
+                    />
+                    <button className="button mt-6">
+                      Subscribe to monthly newsletter
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
-          </>
-        )}
-      </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
